42e3a9f748525fc453d275444c24f4e7656ab378
商盼盼 <shangpanpan@shangpanpandeMacBook-Pro.local>
2018-05-17 16:58:03 +0800

    修改.eslintrc.js文件

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -19,10 +19,7 @@ module.exports = {
   {{/if_eq}}
   {{#if_eq lintConfig "eslint-config-58fe"}}
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    // https://github.com/RekingZhang/eslint-config-58fe
     '58fe'
   ],
   {{/if_eq}}
