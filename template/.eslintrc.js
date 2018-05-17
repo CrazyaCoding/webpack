d6988f4d471c31165fe1d9e8d3a36be38d32564d
商盼盼 <shangpanpan@shangpanpandeMacBook-Pro.local>
2018-05-17 16:23:29 +0800

    增加58fe的eslint规则

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -17,6 +17,15 @@ module.exports = {
     'standard'
   ],
   {{/if_eq}}
+  {{#if_eq lintConfig "eslint-config-58fe"}}
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    '58fe'
+  ],
+  {{/if_eq}}
   {{#if_eq lintConfig "airbnb"}}
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
